c454e048c9e3728a77000856250007fc3cb0feb1
James Ramirez <james.ramirez@grendel-consulting.com>
2023-01-21 17:21:10 +0000

    build(deps): use pnpm over yarn

---
+++ app/routes/index.tsx
@@ -0,0 +1,18 @@
+export default function Index() {
+  return (
+    <div style={{ fontFamily: "system-ui, sans-serif", lineHeight: "1.4" }}>
+      <h1>Welcome to Remix</h1>
+      <ul>
+        <li>
+          <a
+            target="_blank"
+            href="https://www.grendel-consulting.com"
+            rel="noreferrer"
+          >
+            Grendel
+          </a>
+        </li>
+      </ul>
+    </div>
+  );
+}
