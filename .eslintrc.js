17fb311539228910e2d32ffcf24907133cc1e534
James Ramirez <james.ramirez@grendel-consulting.com>
2023-03-04 19:37:13 +0000

    build: begin tooling with basic linting, formatting and testing

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,4 +1,18 @@
-/** @type {import('eslint').Linter.Config} */
 module.exports = {
-  extends: ["@remix-run/eslint-config", "@remix-run/eslint-config/node"],
+  env: {
+    browser: true,
+    es2021: true,
+  },
+  extends: [
+    "@remix-run/eslint-config",
+    "@remix-run/eslint-config/node",
+    "plugin:storybook/recommended",
+  ],
+  overrides: [],
+  parser: "@typescript-eslint/parser",
+  parserOptions: {
+    ecmaVersion: "latest",
+  },
+  plugins: ["react"],
+  rules: {},
 };
