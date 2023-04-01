420da3b93a967126421d79832837730ed517abf3
James Ramirez <james.ramirez@grendel-consulting.com>
2023-04-01 06:51:25 +0100

    chore(deps): finish storybook v7 migration

--- .storybook/main.ts
+++ .storybook/main.ts
@@ -1,18 +1,13 @@
 import type { StorybookConfig } from "@storybook/react-vite";
 const config: StorybookConfig = {
   stories: ["../stories/**/*.mdx", "../stories/**/*.stories.@(js|jsx|ts|tsx)"],
-  addons: [
-    "@storybook/addon-a11y",
-    "@storybook/addon-essentials",
-    "@storybook/addon-interactions",
-    "@storybook/addon-links",
-  ],
+  addons: ["@storybook/addon-a11y", "@storybook/addon-essentials", "@storybook/addon-interactions", "@storybook/addon-links", "@storybook/addon-mdx-gfm"],
   framework: {
     name: "@storybook/react-vite",
-    options: {},
+    options: {}
   },
   docs: {
-    autodocs: "tag",
-  },
+    autodocs: "tag"
+  }
 };
 export default config;
